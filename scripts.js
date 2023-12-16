c0fd8d8218f50783690b50c6f5cec1f2d1f72ae9
Guilherme Leite <128203474+NewGuiLeite@users.noreply.github.com>
2023-12-16 00:50:26 -0300

    Correcoes

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@ const modalOverlay = document.querySelector('.modal_overlay')
 const modal = document.querySelector('.modal')
 const cards = document.querySelectorAll('.card')
 
-for(let card of cards){
-    card.addEventListener('click', () =>{
+for (let card of cards) {
+    card.addEventListener('click', () => {
         cardId = card.getAttribute('id')
         cardName = card.querySelector('p.nome').innerHTML
         cardChef = card.querySelector('p.chef').innerHTML
@@ -20,3 +20,21 @@ document.querySelector('.modal_close').addEventListener('click', () => {
     modalOverlay.querySelector('h1').innerHTML = null
     modalOverlay.querySelector('p').innerHTML = null
 })
+
+function searchRecipes() {
+    // Adicione o código de pesquisa aqui
+    var searchTerm = document.getElementById('searchInput').value.toLowerCase();
+
+    // Iterar sobre os cards para verificar correspondências
+    for (let card of cards) {
+        const cardName = card.querySelector('p.nome').innerHTML.toLowerCase();
+        const cardChef = card.querySelector('p.chef').innerHTML.toLowerCase();
+
+        // Se o termo de pesquisa estiver no nome ou no chef, mostrar o card
+        if (cardName.includes(searchTerm) || cardChef.includes(searchTerm)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    }
+}
